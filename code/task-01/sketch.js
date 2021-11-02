422274388cd407e974cc0e6b5998e3b0a73eb993
Hermann Plock <Hermann.plock@gmx.de>
2021-11-02 21:47:31 +0100

    Create sketch.js

---
+++ code/task-01/sketch.js
@@ -0,0 +1,68 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  background(200, 120, 200);
+
+  stroke(0, 0, 0);
+  strokeWeight(10);
+
+  //circle(200, 300, 30) 
+  ellipse (200, 300, 30, 10)
+  ellipse (200, 350, 30, 10)
+  
+  point(210, 210);
+
+  point(300, 200);
+
+  
+  //point(100, 100);
+  //point(300, 100);
+
+  point(200, 300);
+  
+  
+  
+
+  stroke(0, 0, 0);
+  strokeWeight(5);
+
+  line(218, 350, 218, 300)
+  line(182, 350, 182, 300)
+  
+  rect (120, 120, 200, 100)
+  rect (120, 120, 200, 100)
+  
+  rect (140, 220, 20, 30)
+  rect (170, 220, 20, 30)
+  
+  rect (280, 220, 20, 30)
+  rect (250, 220, 20, 30)
+  
+  stroke(0, 0, 0);
+  strokeWeight(15);
+  point(260, 150);
+  point(160, 150)
+
+  stroke(0, 0, 0);
+  strokeWeight(10);
+  ellipse (215, 180, 150, 10)
+  
+  stroke(0, 0, 0);
+  strokeWeight(5);
+  line(150, 200, 200, 300)
+  //square (100, 100, 100)
+  
+
+  stroke(0, 0, 0);
+  strokewheight(10)
+  point (120, 120)
+
+  line(150, 200, 200, 300)
+
+  arc (300, 300, 100, 100)
+}
+
+
