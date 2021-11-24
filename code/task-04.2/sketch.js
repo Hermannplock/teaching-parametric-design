4bcccb06853c7e6339e042ad622129596cc9254d
Hermann Plock <Hermann.plock@gmx.de>
2021-11-24 10:04:49 +0100

    .

---
+++ code/task-04.2/sketch.js
@@ -0,0 +1,18 @@
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+  background(200);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  
+
+  
+ endShape(CLOSE);
+}
+
+
