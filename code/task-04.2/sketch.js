194158a9262498ba753566f541b2c41bb6718312
Hermann Plock <Hermann.plock@gmx.de>
2021-11-24 10:51:00 +0100

    Update sketch.js

--- code/task-04.2/sketch.js
+++ code/task-04.2/sketch.js
@@ -3,16 +3,38 @@ const sketchWidth = 400;
 
 
 function setup() {
-  createCanvas(sketchWidth, sketchHeight);
-  background(200);
-  angleMode(DEGREES);
+  createCanvas(sketchWidth, sketchHeight,);
+
 }
 
-function draw() {
-  
 
-  
- endShape(CLOSE);
-}
+
+function draw(){
+ noLoop();
+ background(255);
+ fill (240);
+ 
+
+ 
+ for (let i= 70; i <= 340; i += 60 ){
+   let circleRadius = 60 + i * 0.18
+   circle (70, i, circleRadius);
+  }
+   
+  for (let i= 70; i <= 340; i += 60 ){
+    let circleRadius = 60 + i * 0.18 
+    circle (330, i, circleRadius);
+   
+  }
+
+  for (let i= 90; i <= 340; i += 60 ){
+    let circleRadius = 125 - i * 0.18
+    rotate 
+    circle (200, i, circleRadius);
+
+    save ('circles.png')
+  }
+} 
+
 
 
